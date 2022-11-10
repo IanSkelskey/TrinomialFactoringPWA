35d9a18575cc3a505c6f3284c85e7909c5193cdb
Ian Skelskey <ianskelskey@gmail.com>
2022-11-10 02:14:50 -0500

    Test more math visuals

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TrinomialFactoringPWA
+# Trinomial Factoring PWA
 A web app that factors trinomial quadratics and displays the steps.
--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <h2>Coming soon!</h2>
       </header>
       <div className='App-content'>
+        <Field></Field>
         <MathJaxContext >
           <MathJax>
             <h4>Standard Form for a Quadratic Trinomial</h4>
@@ -36,17 +37,70 @@ function App() {
 }
 
 function Field() {
-  const [coefficient, setCoefficient] = useState("a");
+  const [coefficients, setCoefficients] = useState(["a", "b", "c"]);
+
+  const setA = event => {
+    setCoefficients([event.target.value, coefficients[1], coefficients[2]]);
+  }
+  const setB = event => {
+    setCoefficients([coefficients[0], event.target.value, coefficients[2]]);
+  }
+  const setC = event => {
+    setCoefficients([coefficients[0], coefficients[1], event.target.value]);
+  }
+
+  let equation = "\\(" + coefficients[0] + "x^{2}+" + coefficients[1] + "x+" + coefficients[2] + "\\)"
 
   return (
     <div>
-      <form className='form'>
-        <label>
-          {"a: "}
-        </label>
-        <textarea value={coefficient} />
-        <input type="submit" value="Factor!" />s
-      </form>
+      <MathJaxContext>
+        <MathJax>
+
+          <div className='math'>
+            {
+              equation
+            }
+          </div>
+        </MathJax>
+        <form className='form'>
+          <h4>Enter the coeffecients!</h4>
+          <div className='row'>
+            <label>
+              {"a: "}
+              <textarea value={coefficients[0]} onChange={setA} />
+            </label>
+            <label>
+              {"b: "}
+              <textarea value={coefficients[1]} onChange={setB} />
+            </label>
+            <label>
+              {"c: "}
+              <textarea value={coefficients[2]} onChange={setC} />
+            </label>
+          </div>
+
+          <input type="submit" value="Factor!" />
+        </form>
+        <h4>Steps</h4>
+        <ol>
+          <li>
+            <MathJax>
+              {"\\(x^{2} + 5x + 5x + 25\\)"}
+            </MathJax>
+          </li>
+          <li>
+            <MathJax>
+              {"\\(x(x+5)+ 5(x + 5)\\)"}
+            </MathJax>
+          </li>
+          <li>
+            <MathJax>
+              {"\\((x+5)(x+5)\\)"}
+            </MathJax>
+          </li>
+        </ol>
+      </MathJaxContext>
+
     </div>
 
   );
